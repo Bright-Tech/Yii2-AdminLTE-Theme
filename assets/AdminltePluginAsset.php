7545af2e85254891f0fc8a4ce802f36d002bf696
Sam.Xiao <samxiaotj@gmail.com>
2016-10-09 17:20:24 +0800

    增加 wysihtml5 样式

--- assets/AdminltePluginAsset.php
+++ assets/AdminltePluginAsset.php
@@ -16,10 +16,13 @@ class AdminltePluginAsset extends AssetBundle
 
     public $sourcePath = '@vendor/almasaeed2010/adminlte/plugins';
     public $css = [
-        'iCheck/square/blue.css'
+        'iCheck/square/blue.css',
+        'bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css'
     ];
     public $js = [
-        'iCheck/icheck.min.js'
+        'iCheck/icheck.min.js',
+        'bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
+        'slimScroll/jquery.slimscroll.min.js'
     ];
 
     public $depends = [
